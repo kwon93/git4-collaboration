9c296d554050b0924f92970311a728f6e8667334
kdh93 <93umsongaji@gmail.com>
2023-08-07 10:38:15 +0900

    spring start

---
+++ gitExample/src/test/java/green/git/gitExample/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package green.git.gitExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

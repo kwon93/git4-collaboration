8484600c68ca8abef3ea9d5a7a0dbbcc778e1f78
kdh93 <93umsongaji@gmail.com>
2023-08-17 09:36:40 +0900

    merge

--- gitExample/src/main/java/green/git/gitExample/GitExampleApplication.java
+++ gitExample/src/main/java/green/git/gitExample/GitExampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitExampleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GitExampleApplication.class, args);
 	}

52283bf987d39e2b0b72c2a792576e62b372297c
kdh93 <93umsongaji@gmail.com>
2023-08-17 11:02:31 +0900

    test

--- gitExample/src/main/java/green/git/gitExample/controller/GitController.java
+++ gitExample/src/main/java/green/git/gitExample/controller/GitController.java
@@ -1,9 +1,15 @@
 package green.git.gitExample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GitController {
+
+    @GetMapping
+    public void test(){
+
+    }
 
 
 }
